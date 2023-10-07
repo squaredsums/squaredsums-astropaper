35c35bca5e5333c463b1306e8df61c913dae17ff
Matthias Ragus <matt@squaredsums.com>
2023-10-07 19:51:38 +0200

    Updated index page and header

---
+++ src/components/Landing.md
@@ -0,0 +1,70 @@
+---
+layout: ../layouts/Landing.astro
+title: "Crafting The Data-Driven Future"
+---
+
+## How we can help
+
+We combine our data expertise with tailored strategies to propel your business into the future of data-driven success
+
+### Building robust data infrastructures&nbsp;🛠️
+
+- Architect and manage **scalable data systems**, ensuring optimal data availability,
+  integrity, and security.
+- Refine **data pipelines** for top-tier performance and cost-effectiveness.
+
+### Developing data strategies&nbsp;🚀
+
+- Craft and champion the transition towards a **data-driven culture**.
+- Foster comprehensive **data strategies** that guide future direction and bolster
+  processes.
+
+### Empowering through actionable insights&nbsp;💡
+
+- Design **cutting-edge analytics**, tailored KPIs, and reporting solutions.
+- Develop **visualization** tools with leading BI solutions such as Power BI, Looker,
+  and Tableau.
+- Turn complex data into **actionable insights** that empower businesses.
+
+### Implementing data governance&nbsp;🛡️
+
+- Reinforce data governance protocols for top-notch **data quality**.
+- Ensure rigorous **data security** measures and protocols.
+- Prioritize and maintain stringent **compliance standards**.
+
+### Redefining business processes with AI and machine learning&nbsp;🤖
+
+- Deploy state-of-the-art **machine learning models** to extract deep insights from data.
+- Design and implement **AI-driven solutions** to solve complex business challenges.
+- Harness the potential of AI to **automate tasks**, enhance decision-making, and
+  optimize customer service.
+
+### Bridging gaps & building teams&nbsp;🤝
+
+- Facilitate **seamless internal and external communication** with diverse clientele.
+- Mentor and uplift a high-performing cadre of **data professionals**.
+
+### Elevating sales with data-driven precision&nbsp;🎯
+
+- Harness the power of **sales KPIs** to drive tangible growth and maximize revenue.
+- Utilize data analytics to identify sales trends, forecast opportunities, and **optimize
+  conversion** strategies.
+
+### Leveraging data in digital marketing&nbsp;📣
+
+- Harness the power of data to craft impactful **data-driven marketing strategies**.
+- Engage audiences, **drive conversions**, and elevate brand presence in the digital
+  space.
+
+---
+
+## Contact Us
+
+Ready to transform your business with data-driven solutions?
+
+🚀 Email us at: dev@squaredsums.com
+
+Squared Sums Pte. Ltd.\
+160 Robinson Road\
+#14-04 Singapore Business Federation Centre\
+Singapore 068914
--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
-  ogImage: "astropaper-og.jpg",
+  website: "https://squaredsums.com/",
+  author: "Matthias Ragus",
+  desc: "Pioneers in Data-Driven Business Transformation",
+  title: "Squared Sums",
+  ogImage: "squaredsums-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
 };
@@ -13,131 +13,35 @@ export const SITE: Site = {
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
 
 export const LOGO_IMAGE = {
-  enable: false,
+  enable: true,
   svg: true,
-  width: 216,
-  height: 46,
+  width: 235,
+  height: 35,
 };
 
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/squaredsums",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
   {
-    name: "Facebook",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Facebook`,
-    active: true,
-  },
-  {
-    name: "Instagram",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Instagram`,
-    active: true,
-  },
-  {
     name: "LinkedIn",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://www.linkedin.com/company/squaredsums/",
     linkTitle: `${SITE.title} on LinkedIn`,
     active: true,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:dev@squaredsums.com",
     linkTitle: `Send an email to ${SITE.title}`,
-    active: false,
+    active: true,
   },
   {
     name: "Twitter",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Twitter`,
-    active: false,
-  },
-  {
-    name: "Twitch",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Twitch`,
-    active: false,
-  },
-  {
-    name: "YouTube",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on YouTube`,
-    active: false,
-  },
-  {
-    name: "WhatsApp",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on WhatsApp`,
-    active: false,
-  },
-  {
-    name: "Snapchat",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Snapchat`,
-    active: false,
-  },
-  {
-    name: "Pinterest",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Pinterest`,
-    active: false,
-  },
-  {
-    name: "TikTok",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on TikTok`,
-    active: false,
-  },
-  {
-    name: "CodePen",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on CodePen`,
-    active: false,
-  },
-  {
-    name: "Discord",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Discord`,
-    active: false,
-  },
-  {
-    name: "GitLab",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on GitLab`,
-    active: false,
-  },
-  {
-    name: "Reddit",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Reddit`,
-    active: false,
-  },
-  {
-    name: "Skype",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Skype`,
-    active: false,
-  },
-  {
-    name: "Steam",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Steam`,
-    active: false,
-  },
-  {
-    name: "Telegram",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Telegram`,
-    active: false,
-  },
-  {
-    name: "Mastodon",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Mastodon`,
     active: false,
   },
 ];
